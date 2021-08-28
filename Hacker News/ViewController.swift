45c2d435586da22954ddff8f3d5b07669975c4ca
Mohamed Khater <mohamed.15751@gmail.com>
2021-08-28 01:33:46 -0700

    Initial Commit

---
+++ Hacker News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hacker News
+//
+//  Created by Khater on 8/28/21.
+//  Copyright © 2021 Khater. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
